772e9413b88c33303e7c6986be97877816873069
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-06-26 16:20:21 -0500

    ADD USER FUNCTIONALITY
    - added a personal note model that inherits from note
    - registered models in the admin.py file

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     id=models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -9,3 +10,6 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     category = models.CharField(max_length=20)
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
