45316f65b6f96aac6d3e897e6c5f3c2f45ba22cd
kait-schorr <kaitlynschorr@gmail.com>
2018-06-27 16:53:20 -0500

    IMPLEMENT PERSONAL BOOKMARK SEPARATION
    - changed personal bookmarks context to  only showed logged in user
    - anonymous users returns an empty object set

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
 # Create your views here.
 
 
 def index(request):
-    context = {'bookmarks': Bookmark.objects.all()}
+    context = {'public_bookmarks': Bookmark.objects.all()}
+    if request.user.is_anonymous:
+        context['private_bokomarks'] = PersonalBookmark.objects.none()
+    else:
+        context['private_bookmarks'] = PersonalBookmark.objects.filter(
+            user=request.user)
     return render(request, 'bookmarks/index.html', context)
