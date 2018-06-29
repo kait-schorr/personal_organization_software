c6bd522f2bef5e533a57830ddf9a091d27b2d7e7
kait-schorr <kaitlynschorr@gmail.com>
2018-06-29 13:45:08 -0500

    DEBUGGING

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from decouple import config
 
-import dj_database_url
 
-DATABASES = {}
+DATABASES = {'default': config('DATABASE_URL')}
 DATABASES['default'] = dj_database_url.config(default='postgres://...')
 
 
